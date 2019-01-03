a7cda90e27842f76e8dd6bd54d75d7b379bdf9f4
onefemi <onefemi@gmail.com>
2019-01-03 15:05:44 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,49 @@
+var express =  require('express');
+var session = require('cookie-session');
+var bodyParser = require('body-Parser');
+var urlencodedParser = require('urlencoded-parser'); // ES5
+
+var app = express();
+app.use(session({secret: 'todotopsecret'}))
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(function(req,res,next){
+  if (typeof(req.session.todolist) == 'undefined') {
+    req.session.todolist = [];
+  }
+next();
+})
+
+.get('/todo', function(req, res) {
+    res.render('todo.ejs', {todolist: req.session.todolist});
+})
+
+/* Adding an item to the to do list */
+.post('/todo/add/',urlencodedParser,function(req, res) {
+  console.log('adding to list');
+    if (req.body.newtodo != '') {
+        req.session.todolist.push(req.body.newtodo);
+    }
+    res.redirect('/todo');
+})
+
+.get('/todo/update/:id/:eds',function(req, res) {
+    //console.log(req.params.eds);
+    req.session.todolist[req.params.id] = req.params.eds;
+    res.redirect('/todo');
+})
+
+.get('/todo/delete/:id', function(req, res) {
+    if (req.params.id != '') {
+        req.session.todolist.splice(req.params.id, 1);
+    }
+    res.redirect('/todo');
+})
+
+/* Deletes an item from the to do list */
+
+.use(function(req, res, next){
+    res.redirect('/todo');
+})
+app.listen(8080);
